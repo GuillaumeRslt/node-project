76e6b41c3fe44c3eaa8093225f39c47b46c1c36f
GuillaumeRslt <guillaume.rousselet@etu.unilim.fr>
2021-03-29 23:15:35 +0200

    structure favorite and movie

--- server/manifest.js
+++ server/manifest.js
@@ -47,10 +47,10 @@ module.exports = new Confidence.Store({
                         knex: {
                             client: 'mysql',
                             connection: {
-                                host: process.env.DB_HOST || '127.0.0.1',
-                                user: process.env.DB_USER || 'root',
-                                password: process.env.DB_PASSWORD || '',
-                                database: process.env.DB_DATABASE || 'hapi-user'
+                                host: process.env.DB_HOST,
+                                user: process.env.DB_USER,
+                                password: process.env.DB_PASSWORD,
+                                database: process.env.DB_DATABASE
                             }
                         }
                     },
